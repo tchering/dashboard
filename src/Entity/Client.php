93ee228e8a07ff95d32d5c29c82e5acf0319af07
tchering <sonam.sherpa.lama@gmail.com>
2024-02-18 13:37:07 +0100

    fixed margin for rightbox

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -2,10 +2,11 @@
 
 namespace App\Entity;
 
-use App\Repository\ClientRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
+use App\Entity\Commande;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\ClientRepository;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 
 #[ORM\Entity(repositoryClass: ClientRepository::class)]
 class Client
